c4295e29f1e444bf8e1f88a9fbec1badab7ab9cd
Muhammad Ikhsan Morgan Nurfiantoro <150980349+M0R6@users.noreply.github.com>
2025-06-02 20:54:26 +0700

    Initial commit

---
+++ components/Footer.vue
@@ -0,0 +1,70 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p class="mb-2">
+        Feel free to reach out via email at <a class="hover:text-blue-300 font-black text-xl" href="mailto:halomrgn@gmail.com">halomrgn@gmail.com</a>.<br>
+      </p>
+      <div class="social-icons">
+        <a href="https://instagram.com/nrfntr" target="_blank">
+          <font-awesome-icon class="h-6 mx-auto cursor-pointer text-white hover:text-blue-300"  :icon="['fab', 'instagram']" />
+        </a>
+        <a href="https://x.com/nrfntr" target="_blank">
+          <font-awesome-icon class="h-6 mx-auto cursor-pointer text-white hover:text-blue-300" :icon="['fab', 'x-twitter']" />
+        </a>
+        <a href="https://youtube.com/@M0R6" target="_blank">
+          <font-awesome-icon class="h-6 mx-auto cursor-pointer text-white hover:text-blue-300" :icon="['fab', 'youtube']" />
+        </a>
+        <a href="https://open.spotify.com/artist/7DPc5JGMWxo7gyHxIvfyHW?si=YMuVGHZiTT67pAK1R2fp0g" target="_blank">
+          <font-awesome-icon class=" h-6 mx-auto cursor-pointer text-white hover:text-blue-300" :icon="['fab', 'spotify']" />
+        </a>
+        <a href="https://soundcloud.com/nrfntr" target="_blank">
+          <font-awesome-icon class="h-6 mx-auto cursor-pointer text-white hover:text-blue-300"  :icon="['fab', 'soundcloud']" />
+        </a>
+        <a href="https://twitch.tv/nrfntr" target="_blank">
+          <font-awesome-icon class="h-6 mx-auto cursor-pointer text-white hover:text-blue-300" :icon="['fab', 'twitch']" />
+        </a>
+        <a href="https://discord.com/users/944014822603907102" target="_blank">
+          <font-awesome-icon class="h-6 mx-auto cursor-pointer text-white hover:text-blue-300" :icon="['fab', 'discord']" />
+        </a>
+      </div>
+      <p>
+        &copy; 2024 <a class="hover:text-blue-300" href="https://morgann.me/" target="_blank">MORG</a><br>
+        Background picture by <a class="hover:text-blue-300" href="https://unsplash.com/@introspectiveds" target="_blank">Eric Mclean</a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: #212121;
+  color: #fff;
+  padding: 1rem 0;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  text-align: center;
+}
+.social-icons {
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+}
+.social-icons a {
+  color: #fff;
+  margin: 0 0.5rem;
+  transition: color 0.3s;
+}
+.social-icons a:hover {
+  color: #1da1f2; /* Change this to your desired hover color */
+}
+.social-icon {
+  font-size: 1px; /* Adjust this value as needed */
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="josef">
+    <div id="disini" class="w-screen h-[56px] border-0 p-1 m-0 bg-black" :class="{ 'hidden': hidden }">
+      <div @click="hideIt" class="cursor-pointer">
+        <div class="inline-block p-2">
+          <font-awesome-icon class="h-6 ml-1 mt-1 cursor-pointer text-white" icon="xmark" />
+        </div>
+        <a href="https://sndo.ffm.to/55mdjb6" target="_blank">
+          <div class="mt-[-56px] text-center p-[12px] text-2xl">
+            <h1 class="text-white">
+              <span>Stream</span>
+              <span class="text-blue-300">MORG's "Wait On You"</span>
+              <span>now!</span>
+            </h1>
+          </div>
+        </a>
+      </div>
+    </div>
+    <nav class="w-full bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white py-5 px-4 shadow-lg">
+      <div class="container mx-auto flex justify-center items-center">
+      <ul class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-12 text-center">
+        <li>
+        <a href="#spotify" class="relative font-medium text-lg hover:text-green-400 transition-colors duration-300 group">
+          Spotify
+          <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-green-400 group-hover:w-full transition-all duration-300"></span>
+        </a>
+        </li>
+        <li>
+        <a href="#youtube" class="relative font-medium text-lg hover:text-red-400 transition-colors duration-300 group">
+          YouTube
+          <span class="absolute left-0 bottom-0 w-0.5 bg-red-400 group-hover:w-full transition-all duration-300"></span>
+        </a>
+        </li>
+        <li>
+        <a href="#contact" class="relative font-medium text-lg hover:text-purple-400 transition-colors duration-300 group">
+          Contacts
+          <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-purple-400 group-hover:w-full transition-all duration-300"></span>
+        </a>
+        </li>
+      </ul>
+      </div>
+    </nav>
+    <div class="bg-container mx-auto w-screen h-screen border-0 p-0 m-0" :style="backgroundStyle">
+      <div class="flex-auto text-center text-4xl">
+        <a href="">
+          <!-- <h1 class="pt-[15px] text-white josef2">
+            <span>Morgan</span> <br />
+            <span>Room</span> <br />
+            <span>Records</span>
+          </h1> -->
+          <v-img class="mx-auto mt-3 d-flex justify-center" height="300" width="300" :src="require('~/assets/images/mrr.svg')"></v-img>
+        </a>
+      </div>
+      <div class="mx-auto justify-center flex">
+        <!-- <iframe width="560" height="202" src="https://www.bandlab.com/embed/?id=77dde34a-1640-ef11-86c3-000d3a42581b" allowfullscreen></iframe> -->
+      </div>
+      <div>
+        <h1 class="pt-10 text-center text-3xl text-white josef2">MORG's Music</h1>
+      </div>
+      <div id="spotify">
+        <h1 class="pt-10 text-center text-3xl text-white josef2">Listen on Spotify</h1>
+        <div class="flex justify-center">
+          <iframe
+            src="https://open.spotify.com/embed/track/73TDTh7iFO0mdhfLs8lokn?utm_source=generator"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+            class="rounded-lg pt-3"
+          ></iframe>
+        </div>
+      </div>
+      <div id="youtube">
+        <h1 class="pt-10 text-center text-3xl text-white josef2">Latest YouTube Videos</h1>
+        <YouTubeVideos :videos="videos"/>
+      </div>
+      <!-- <div>
+        <h1 class="pt-10 text-center text-3xl text-white josef2">Tracks on BandLab</h1>
+        <BandLabEmbed :tracks="tracks" />
+      </div> -->
+    </div>
+    <footer id="contact">
+      <Footer />
+    </footer>
+  </div>
+</template>
+
+<style>
+.bg-container {
+  height: 100vh;
+  width: 100vw;
+  overflow: auto; /* Ensure content can scroll */
+  /* background-color: black;  */
+}
+</style>
+
+<script>
+import YouTubeVideos from "@/components/YouTubeVideos.vue";
+import BandLabEmbed from "@/components/BandLabEmbed.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    YouTubeVideos,
+    BandLabEmbed,
+    Footer,
+  },
+  data() {
+    return {
+      hidden: false,
+      isModalVisible: false,
+      selectedVideoUrl: '',
+      videos: [
+        { id: "4At0dI8xJa4" },
+        { id: "Wz9BlivgSbo" },
+        { id: "KG5fkBOLg4c" },
+        { id: "-Zm_VqCsCCY" },
+        { id: "15xQoUNMWVc" },
+        { id: "4Sqw-GwACNI" },
+        { id: "E2OZb9yknjU" },
+        { id: "b3KM9OthMoA" },
+        { id: "-W01JTUmr9s" },
+        { id: "djzBGrzEWzI" },
+        { id: "_CUD9lIzxBo" },
+        { id: "80DxoS3t9vg" },
+        { id: "KluIuQGYeM0" },
+        { id: "pcrVyJj6CYk" },
+      ],
+      tracks: [
+        { id: "77dde34a-1640-ef11-86c3-000d3a42581b" },
+        // Add more BandLab track IDs as needed
+      ],
+    };
+  },
+  computed: {
+    backgroundStyle() {
+      return {
+        backgroundImage: `url(${require('~/assets/images/bg.jpg')})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover',
+      };
+    },
+  },
+  methods: {
+    hideIt() {
+      this.hidden = !this.hidden;
+    },    
+  },
+  mounted() {
+    // Force reactivity to ensure background image is applied
+    this.$forceUpdate();
+  },
+};
+</script>
